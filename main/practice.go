c4be518ae877b279c35370a33dd112dd010354d2
ss7m <Sean.Y.Shu@newegg.com>
2019-04-06 09:41:38 +0800

    aaa

--- main/practice.go
+++ main/practice.go
@@ -1,40 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+type Stack []string
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
+func (s *Stack) Pop() string {
+	n := len(*s)
+	v := (*s)[n-1]
+	*s = (*s)[:n-1]
+	return v
 }
 
-func main() {
-	var a, b, c, d = new(ListNode), new(ListNode), new(ListNode), new(ListNode)
-	a.Val = 1
-	a.Next = b
-	b.Val = 2
-	b.Next = c
-	c.Val = 3
-	c.Next = d
-	d.Val = 4
-	fmt.Printf("%d->%d->%d->%d\n", a.Val, b.Val, c.Val, d.Val)
-	result := swapPairs(a)
-	fmt.Printf("%d->%d->%d->%d", result.Val, result.Next.Val, result.Next.Next.Val, result.Next.Next.Next.Val)
-}
-
-func swapPairs(head *ListNode) *ListNode {
-	var pre = new(ListNode)
-	if head == nil || head.Next == nil {
-		return head
+func isValid(s string) bool {
+	var slice = Stack{}
+	var kMap = map[string]string{"}": "{", "]": "[", ")": "("}
+	for _, v := range s {
+		if _, ok := kMap[string(v)]; !ok {
+			slice = append(slice, string(v))
+		} else if len(slice) == 0 || slice.Pop() != kMap[string(v)] {
+			return false
+		}
 	}
-	newHead := head.Next
-	for head != nil && head.Next != nil {
-		a := head
-		b := head.Next
-		pre.Next, a.Next, b.Next, pre = b, b.Next, a, a
-		head = a.Next
-	}
-
-	return newHead
+	return len(slice) == 0
 }
