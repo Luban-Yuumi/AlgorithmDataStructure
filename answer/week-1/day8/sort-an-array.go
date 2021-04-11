918c75c0fcf809827ba500f00fce55dd03e6a7ef
shuning <1850816020@qq.com>
2021-04-11 15:04:15 +0800

    alert sort arr

--- answer/week-1/day8/sort-an-array.go
+++ answer/week-1/day8/sort-an-array.go
@@ -1,57 +1,48 @@
-package day8
+package main
 
-import "fmt"
+func sortArray(nums []int) []int {
+	//quickSort(nums, 0, len(nums)-1)
+	maoPaoSort(nums)
+	return nums
+}
 
-//https://www.cnblogs.com/nima/p/12724868.html
-
-func quickSort(nums []int, i, j int) {
-	begin := partition(i, j, nums)
-	if begin < 0 {
+func quickSort(nums []int, begin, end int) {
+	if begin >= end {
 		return
 	}
-
-	quickSort(nums, i, begin-1)
-	quickSort(nums, begin+1, j)
-
-}
-
-func partition(i, j int, nums []int) int {
-	begin := i + 1
-	end := j
-	if begin > end {
-		return -1
-	}
-	for begin < end {
-		if nums[begin] > nums[i] {
-			nums[begin], nums[end] = nums[end], nums[begin]
-			end--
+	i := begin + 1
+	j := end
+	for j > i {
+		if nums[i] > nums[begin] {
+			nums[i], nums[j] = nums[j], nums[i]
+			j--
 		} else {
-			begin++
+			i++
 		}
 	}
-
-	if nums[begin] > nums[i] {
-		begin = begin - 1
+	mid := 0
+	if nums[i] > nums[begin] {
+		mid = i - 1
+	} else {
+		mid = i
 	}
-
-	nums[i], nums[begin] = nums[begin], nums[i]
-	return begin
+	nums[begin], nums[mid] = nums[mid], nums[begin]
+	quickSort(nums, begin, mid-1)
+	quickSort(nums, mid+1, end)
 }
 
-func main() {
-	list := []int{5}
-	quickSort(list, 0, len(list)-1)
-	fmt.Println(list)
+func maoPaoSort(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		change := false
+		for j := 0; j < len(nums)-i-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+				change = true
+			}
+		}
 
-	list1 := []int{5, 9}
-	quickSort(list1, 0, len(list1)-1)
-	fmt.Println(list1)
-
-	list2 := []int{5, 9, 1}
-	quickSort(list2, 0, len(list2)-1)
-	fmt.Println(list2)
-
-	list3 := []int{5, 9, 1, 6, 8, 14, 6, 49, 25, 4, 6, 3}
-	quickSort(list3, 0, len(list3)-1)
-	fmt.Println(list3)
+		if !change {
+			break
+		}
+	}
 }
